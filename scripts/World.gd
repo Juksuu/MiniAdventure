edf563970f86ee4ce95974de86e98fade54b5fdd
Frans Paasonen <franspaasonen@gmail.com>
2021-07-20 04:22:40 +0300

    feat: Add potion seller

--- scripts/World.gd
+++ scripts/World.gd
@@ -8,18 +8,25 @@ onready var player = $YSort/Player
 onready var tavern = $YSort/Tavern
 onready var tavern_inside = $YSort/Tavern_inside
 
-func _ready():
-	ysort.remove_child(tavern_inside)
-
-func _on_Tavern_tavern_entered():
+func enter_tavern():
 	ysort.add_child(tavern_inside)
 	
 	map_container.remove_child(map)
 	ysort.remove_child(tavern)
-
-
-func _on_Tavern_inside_tavern_exit():
+	
+func exit_tavern():
 	ysort.remove_child(tavern_inside)
 	
 	map_container.add_child(map)
 	ysort.add_child(tavern)
+
+func _ready():
+	ysort.remove_child(tavern_inside)
+
+func _on_Tavern_tavern_entered():
+	call_deferred("enter_tavern")
+
+
+func _on_Tavern_inside_tavern_exit():
+	call_deferred("exit_tavern")
+
